3fe05a7d3fbb0d20d1ed3dd2dc6022587eeb27f1
sungwoongNoe <asdz453@gmail.com>
2024-08-25 23:40:06 +0900

    feat: spring cloud discovery 서버 추가 및 의존성 설정

---
+++ oompa-cloud-discovery/src/test/java/com/multi/oompaclouddiscovery/OompaCloudDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.oompaclouddiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OompaCloudDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
