d2e6bf794a8a37b94396358f107a1c28a4476399
sungwoongNoe <asdz453@gmail.com>
2024-09-08 14:37:08 +0900

    faet: article 모듈 생성

---
+++ oompa-data-article/src/main/java/com/multi/oompadataarticle/OompaDataArticleApplication.java
@@ -0,0 +1,13 @@
+package com.multi.oompadataarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OompaDataArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OompaDataArticleApplication.class, args);
+	}
+
+}
