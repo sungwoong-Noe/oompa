148e2fdd68adb59420c81db5e4bd600ddf0ba46f
sungwoongNoe <asdz453@gmail.com>
2024-09-26 22:26:33 +0900

    test(article): 게시글 수정 테스트

--- oompa-data-article/src/test/java/com/multi/oompadataarticle/domain/article/ctr/ArticleControllerTest.java
+++ oompa-data-article/src/test/java/com/multi/oompadataarticle/domain/article/ctr/ArticleControllerTest.java
@@ -1,7 +1,10 @@
 package com.multi.oompadataarticle.domain.article.ctr;
 
+import com.multi.oompadataarticle.cmm.status.ArticleStatus;
 import com.multi.oompadataarticle.domain.article.entity.ArticleEntity;
 import com.multi.oompadataarticle.domain.article.repo.ArticleRepository;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -33,6 +37,10 @@ class ArticleControllerTest {
     @Autowired
     ArticleRepository articleRepository;
 
+    @Autowired
+    EntityManager em;
+
+
     @Test
     @DisplayName("Article 저장 요청 - 임시 저장 상태")
     public void create() throws Exception {
@@ -49,7 +57,6 @@ class ArticleControllerTest {
         //expected
         mvc.perform(post("/article")
                         .content(articleForm)
-                        .accept(APPLICATION_JSON)
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content").value(1L))
@@ -61,6 +68,47 @@ class ArticleControllerTest {
         assertThat(all.size()).isEqualTo(1L);
     }
 
+    @Test
+    @DisplayName("Article 수정 요청")
+    public void patch() throws Exception {
+
+        //given
+
+        ArticleEntity article = ArticleEntity.builder()
+                .title("title")
+                .content("content")
+                .status(ArticleStatus.COMPLETE_SAVED)
+                .build();
+
+        articleRepository.saveAndFlush(article);
+
+        em.clear();
 
 
+        String articleForm = """
+                {
+                    "idx": "1",
+                    "title": "update title",
+                    "content": " content",
+                    "status": "TEMP_SAVED"
+                 }               
+                """;
+
+
+        mvc.perform(post("/article")
+                        .content(articleForm)
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value(1L))
+                .andDo(print());
+
+
+        ArticleEntity articleEntity = articleRepository.findById(1L).get();
+        List<ArticleEntity> all = articleRepository.findAll();
+
+        assertThat(articleEntity.getIdx()).isEqualTo(1L);
+        assertThat(articleEntity.getTitle()).isEqualTo("update title");
+        assertThat(all.size()).isEqualTo(1L);
+
+    }
 }
